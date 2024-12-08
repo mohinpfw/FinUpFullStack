c9aa8472bbf5ac477cd8ea2eee940cacef1cfa15
sai vikram gurram <gurrs03@pfw.edu>
2024-12-07 21:35:32 -0500

    vikram commit

---
+++ reactapp/assets/assets.js
@@ -0,0 +1,34 @@
+export const LeaderSVG = () =>{
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Finance%20leaders.svg"} alt={""}/>
+}
+export const ArrowRightIcon = () =>{
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Arrow_right.svg"} alt={""}/>
+}
+export const AddIcon = () =>{
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Add_round.svg"} alt={""}/>
+}
+
+export const AppLogo = () => {
+    return (
+        <div 
+            style={{
+                width: 140, 
+                height: 60, 
+                display: "flex", 
+                alignItems: "center", 
+                justifyContent: "center"
+            }}
+        >
+            <span style={{ color: "#0066ff", fontSize: 24, fontWeight: "bold" }}>FIN</span>
+            <span style={{ color: "black", fontSize: 24, fontWeight: "bold" }}>UP</span>
+        </div>
+    );
+};
+
+export const ArrowGIcon = () =>{
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Arrow_alt_ldown.svg"} alt={""}/>
+}
+
+export const ArrowRIcon = () =>{
+    return <img src={"https://raw.githubusercontent.com/UdhayakumarThangavel/paymint-web-app/main/assets/Arrow_alt_ltop.svg"} alt={""}/>
+}
